c56cb06ba827e967a13af281a67224dbc9928ff9
Stanislav Feldman <stanislavfeldman@gmail.com>
2012-10-01 13:15:14 +0400

    added jquery package inside, you don't need jquery if you use fel.js, its included

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 if(typeof window != "undefined")
 	exports = window;
-var fel = {}
+var fel = {};
 fel.core = require('./lib/core');
 fel.views = require('./lib/views');
 fel.controllers = require("./lib/controllers");
+exports.$ = require("br-jquery");
+exports._ = require("underscore");
 exports.fel = fel;
