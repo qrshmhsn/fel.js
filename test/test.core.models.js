00709bc531ac0aec0a175cdfafa4c63917cb68e9
Stanislav Feldman <stanislavfeldman@gmail.com>
2012-10-20 01:33:47 +0400

    added several test cases

--- test/test.core.models.js
+++ test/test.core.models.js
@@ -7,19 +7,35 @@ describe("core.models.Model", function(){
 		birthday: "date",
 		messages: {model: "Message", many: true, key: "user"}
 	});
-	var user;
-	beforeEach(function(){
+	var Message = fel.models.Model.define("Message",{
+		value: "string",
+		user: {model: "User"} //relation
+	});
+	var user, message, get_user;
+	before(function(){
 		user = new User({id: "stas", age:24, birthday: new Date(), man: true});
 		user.save();
+		message = new Message({id: "my_msg", value: "one more msg", user: user.id});
+		message.save();
 	});
 	describe("get", function(){
+		before(function(){
+			get_user = User.get("stas");
+		});
 		it("should return model by id", function(){
-			var u = User.get(user.id);
-			u.should.exist;
-			u.id.should.be.a("string");
+			get_user.should.exist;
+			get_user.id.should.be.a("string");
+		});
+		it("should return date field as date", function(){
+			get_user.birthday.should.be.a("date");
+		});
+		it("should return many relation field as collection of models", function(){
+			get_user.messages.should.exist;
+			get_user.messages.should.have.length(1);
+			get_user.messages[0].id.should.equal(message.id);
 		});
 	});
-	afterEach(function(){
+	after(function(){
 		User.remove_all();
 	});
 });
