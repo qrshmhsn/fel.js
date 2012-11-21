9a130fb7360201c1dab51b9b13ccb4db3100a88a
Stanislav Feldman <stanislavfeldman@gmail.com>
2012-11-21 17:14:49 +0400

    fixed error when several apps uses one name, you need to specify database name

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #you need node.js, browserify and uglify.js to start it
 #you can run it also as "npm run-script build"
-browserify index.js | uglifyjs -o dist/fel.js
 #browserify index.js -o dist/fel.js
+browserify index.js | uglifyjs -o dist/fel.js
