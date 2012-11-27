141f1a8e2b19ddd8664e33db1d121df81e16342c
Stanislav Feldman <stanislavfeldman@gmail.com>
2012-11-27 15:45:26 +0400

    added more views tests

--- test/test.views.js
+++ test/test.views.js
@@ -5,16 +5,32 @@ describe("views.View", function(){
 	viewer.add_template("test1", "hello, {{name}}");
 	var view1 = new fel.views.View("test1", {name: "stas"});
 	var view1_res = "hello, stas";
+	viewer.add_template("test2", "hi, {{name}}!");
+	var view2 = new fel.views.View("test2", {name: "stas"});
+	var view2_res = "hi, stas!";
 	describe("toString", function(){
 		it("should return rendered string", function(){
 			var res = view1.toString();
 			expect(res).to.equal(view1_res);
 		});
 	});
-	describe("append", function(){
+	describe("render", function(){
 		it("should be in doc body", function(){
+			view1.render("#views");
+			expect($("#views").text()).to.equal(view1_res);
+			$("#views").empty();
+		});
+	});
+	describe("append", function(){
+		it("should be in doc", function(){
 			view1.append("#views");
 			expect($("#views").text()).to.equal(view1_res);
+			$("#views").empty();
+		});
+		it("should append to existing content", function(){
+			view1.append("#views");
+			view2.append("#views");
+			expect($("#views").text()).to.equal(view1_res+view2_res);
 			$("#views").empty();
 		});
 	});
